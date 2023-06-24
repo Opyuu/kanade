76c333146da18b01d0938c87a4d1a115b81c53e3
Opyu <wayne.jiawei.shi@gmail.com>
2023-06-24 13:54:33 +0100

    base of idle game

--- main.js
+++ main.js
@@ -1,77 +1,33 @@
-const members = [
-    ["Ichika", "Saki", "Honami", "Shiho"], 
-    ["Minori", "Haruka", "Airi", "Shizuku"], 
-    ["Kohane", "An", "Akito", "Toya"], 
-    ["Tsukasa", "Emu", "Nene", "Rui"], 
-    ["Kanade", "Mafuyu", "Ena", "Mizuki"]
-]; // [Group][Member]
-output = []
-var index = 0;
+var crystals = 0;
+var roboCount = 0;
+var perSecond = 0;
 
-const images = {
-    "Ichika": "Ichika.webp",
-    "Saki": "Saki.webp",
-    "Honami": "Honami.webp",
-    "Shiho": "Shiho.webp",
-    "Minori": "Minori.webp",
-    "Haruka": "Haruka.webp",
-    "Airi": "Airi.webp",
-    "Shizuku": "Shizuku.webp",
-    "Kohane": "Kohane.webp",
-    "An": "An.webp",
-    "Akito": "Akito.webp",
-    "Toya": "Toya.webp",
-    "Tsukasa": "Tsukasa.webp",
-    "Emu": "Emu.webp",
-    "Nene": "Nene.webp",
-    "Rui": "Rui.webp",
-    "Kanade": "Kanade.webp",
-    "Mafuyu": "Mafuyu.webp",
-    "Ena": "Ena.webp",
-    "Mizuki": "Mizuki.webp"
-};
-
-for(let group1 = 0; group1 < 5; group1++){
-    for (let member1 = 0; member1 < 4; member1++){
-        for (let group2 = group1 + 1; group2 < 5; group2++){
-            for (let member2 = 0; member2 < 4; member2++){
-                if (members[group1][member1] == "Saki" && members[group2][member2] == "Tsukasa"){continue;}
-                if (members[group1][member1] == "Shiho" && members[group2][member2] == "Shizuku"){continue;}
-                if (members[group1][member1] == "Akito" && members[group2][member2] == "Ena"){continue;}
-                output.push(members[group1][member1] + " " + members[group2][member2]);
-            }
-        }
-    }
+function clicked(){
+    crystals++;
 }
 
-function shuffle(array) {
-    let currentIndex = array.length,  randomIndex;
-  
-    // While there remain elements to shuffle.
-    while (currentIndex != 0) {
-  
-      // Pick a remaining element.
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
+function buy_NeneRobo(){
+    let roboCost = Math.floor(10 * Math.pow(1.1, roboCount));
+    if(crystals >= roboCost){
+        roboCount ++;
+        crystals -= roboCost;
+        document.getElementById('RoboCount').innerHTML = roboCount;  //updates the number of nenerobos for the user
+        document.getElementById('RoboCost').innerHTML = roboCost;  //updates the number of nenerobos for the user
     }
-  
-    return array;
-  }
+    roboCost = Math.floor(10 * Math.pow(1.1, roboCount));
+    document.getElementById('RoboCost').innerHTML = roboCost;
+}
 
-output = shuffle(output);
-console.log(output);
+function calculate_gain(){ // Updates the number of crystals earned per second
+    perSecond = roboCount * 1;
 
-function next(){
-    let ship = output[index];
-    list = ship.split(" ");
-    document.getElementById("image1").src=images[list[0]];
-    document.getElementById("image2").src=images[list[1]];
-    document.getElementById("image1label").innerHTML=list[0];
-    document.getElementById("image2label").innerHTML=list[1];
-    document.getElementById("count").innerHTML="Count: " + (index+1).toString();
-    index++;
+    return perSecond / 60;
+}
+
+window.requestAnimationFrame(gameLoop);
+function gameLoop(){
+    document.getElementById("Counter").innerHTML = Math.floor(crystals).toString();
+    document.getElementById("CPS").innerHTML = perSecond.toString();
+    crystals += calculate_gain();
+    window.requestAnimationFrame(gameLoop);
 }
